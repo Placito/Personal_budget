453c9f96a6227ea1678d77701c45de11697c00dd
Mariana <mariana.placito@gmail.com>
2025-01-13 17:38:45 +0000

    add new categories

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import sys
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (
     QApplication, QWidget, QLabel, QVBoxLayout, QHBoxLayout, QFrame,
-    QProgressBar, QTableWidget, QTableWidgetItem
+    QProgressBar, QTableWidget, QTableWidgetItem, QPushButton, QComboBox, QCalendarWidget
 )
 from PyQt5.QtGui import QPixmap
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 import matplotlib.pyplot as plt
-import numpy as np
+
 
 # Colors
 c0 = "#2e2d2b"  # black
@@ -79,7 +79,7 @@ class MyApp(QWidget):
             else:
                 raise FileNotFoundError
         except FileNotFoundError:
-            label_logo = QLabel("  Personal budget management", self)
+            label_logo = QLabel("  Personal Budget Management", self)
             label_logo.setStyleSheet(f"font: bold 20px Verdana; color: {c4};")
             frame_up_layout = QVBoxLayout(frame_up)
             frame_up_layout.addWidget(label_logo)
@@ -93,19 +93,21 @@ class MyApp(QWidget):
         )
         frame_middle.setFixedHeight(300)
         frame_middle_layout = QHBoxLayout(frame_middle)
+        frame_middle_layout.setContentsMargins(0, 0, 0, 0)  # Set margins for this layout
+        frame_middle_layout.setSpacing(0) 
 
         # Progress Bar and Chart Container
         progress_chart_container = self.create_progress_chart_container()
-        frame_middle_layout.addWidget(progress_chart_container)
+        frame_middle_layout.addWidget(progress_chart_container, alignment=Qt.AlignTop)
 
         # Totals Container
         totals_container = self.create_totals_container()
         frame_middle_layout.addWidget(totals_container)
 
+
         # Circular Chart Container
         circular_container = self.create_circular_container()
         frame_middle_layout.addWidget(circular_container)
-
         return frame_middle
 
     def create_bottom_frame(self):
@@ -133,6 +135,7 @@ class MyApp(QWidget):
     def create_progress_chart_container(self):
         container = QWidget(self)
         container.setFixedWidth(200)
+        container.setFixedHeight(100)
         container.setStyleSheet("border: none;")
         layout = QVBoxLayout(container)
 
@@ -167,32 +170,12 @@ class MyApp(QWidget):
         progress_layout.addWidget(percentage_label)
         layout.addLayout(progress_layout)
 
-        # Chart Section
-        list_categories = ["Income", "Expenses", "Balance"]
-        list_values = [3000, 200, 6000]
-        colors = [c2, c5, c6]
-
-        figure = plt.Figure(figsize=(4, 4), dpi=60)
-        ax = figure.add_subplot(111)
-        ax.bar(list_categories, list_values, color=colors, width=0.9)
-        for i, rect in enumerate(ax.patches):
-            ax.text(
-                rect.get_x() + rect.get_width() / 2,
-                rect.get_height() + 50,
-                f"{list_values[i]}",
-                fontsize=10,
-                ha="center",
-            )
-        ax.set_axisbelow(True)
-        ax.yaxis.grid(color="#EEEEEE")
-        canvas = FigureCanvas(figure)
-        layout.addWidget(canvas)
-
         return container
 
     def create_totals_container(self):
         container = QWidget(self)
-        container.setFixedWidth(200)
+        container.setFixedWidth(210)
+        container.setFixedHeight(200)
         container.setStyleSheet("border: none;")
         layout = QVBoxLayout(container)
 
@@ -262,17 +245,27 @@ class MyApp(QWidget):
 
     def create_tables_container(self):
         container = QWidget(self)
-        container.setFixedWidth(210)
+        container.setFixedWidth(300)
         container.setStyleSheet("border: none;")
         layout = QVBoxLayout(container)
 
-        table_label = QLabel("Income and Expense table:", self)
+        table_label = QLabel("Income and Expenses table:", self)
         table_label.setStyleSheet(f"font: bold 12px Verdana; color: {c0};")
         layout.addWidget(table_label)
 
         table = QTableWidget(10, 4, self)
         table.setHorizontalHeaderLabels(['ID', 'Item', 'Date', 'Amount'])
         layout.addWidget(table)
+
+        list_Items = [[1, "Salary", "2025-01-01", 3000],
+              [2, "Rent", "2025-01-05", -1200],
+              [3, "Groceries", "2025-01-10", -300],
+              [4, "Freelance Work", "2025-01-15", 800]]
+
+        # Populate the QTableWidget
+        for row_idx, row_data in enumerate(list_Items):
+            for col_idx, col_data in enumerate(row_data):
+                table.setItem(row_idx, col_idx, QTableWidgetItem(str(col_data)))
 
         # Adjust column widths to fit the content
         table.resizeColumnsToContents()
@@ -282,14 +275,12 @@ class MyApp(QWidget):
             f"""
             QTableWidget {{
                 background-color: {c9};  /* Background color */
-                alternate-background-color: {c1};  /* Alternate row background */
                 border: 1px solid {c2};  /* Border color */
-                color: black;  /* Text color */
+                color: {c0};
                 font: 8px Verdana;
             }}
             QTableWidget::item {{
                 border: 1px solid {c3};  /* Item border */
-                
                 padding: 5px;  /* Cell padding */
             }}
             QHeaderView::section {{
@@ -301,35 +292,66 @@ class MyApp(QWidget):
             }}
             """
         )
+        return container
+
+    def create_crud_container(self, widget_title):
+        container = QWidget(self)
+        container.setStyleSheet("border: none;")
+        layout = QVBoxLayout(container)
+
+        # Title Label
+        label_crud = QLabel(widget_title.upper(), self)
+        label_crud.setStyleSheet("font: bold 10px Verdana; color: black;")
+        layout.addWidget(label_crud)
+
+        # ComboBox for selecting categories
+        self.combo_category = QComboBox(self)
+        self.combo_category.addItems(self.categories)
+        layout.addWidget(self.combo_category)
+
+        # Add Button
+        self.add_button = QPushButton("Add Category", self)
+        self.add_button.clicked.connect(self.add_category)  # Connect button click to add_category method
+        layout.addWidget(self.add_button)
+
+        # List Widget to display added categories
+        self.list_widget = QListWidget(self)
+        layout.addWidget(self.list_widget)
+
+    def add_category(self):
+        # Get the selected category
+        selected_category = self.combo_category.currentText()
+        if selected_category and selected_category not in self.selected_categories:
+            # Add it to the list and the QListWidget
+            self.selected_categories.append(selected_category)
+            self.list_widget.addItem(selected_category)
+            
+         # Grabbing categories
+        categories = ['Travel', 'Food', 'Entertainment', 'Shopping']
+        # Create and show the widget
+        widget = CategoryInput("Choose and Add Category", categories)
+        widget.resize(300, 200)
+        widget.show()
+
+        # PyQt QComboBox
+        combo_category_expense = QComboBox(container)
+        combo_category_expense.addItems(categories)  # Add categories to the combo box
+        layout.addWidget(combo_category_expense)  # Add the combo box to the layout
+
 
         return container
 
-    def create_crud_container(self, label_text):
+
+    def create_configurations_container(self, widget_title):
         container = QWidget(self)
         container.setFixedWidth(240)
         container.setStyleSheet("border: none;")
         layout = QVBoxLayout(container)
 
-        crud_label = QLabel("Enter new Expenses:", self)
-        crud_label.setStyleSheet(f"font: bold 12px Verdana; color: {c0};")
-        layout.addWidget(crud_label)
-
-        # Add additional components for CRUD functionality here
-        # For example, text input fields, buttons, etc.
-        return container
-
-    def create_configurations_container(self, label_text):
-        container = QWidget(self)
-        container.setFixedWidth(240)
-        container.setStyleSheet("border: none;")
-        layout = QVBoxLayout(container)
-
-        configurations_label = QLabel("Enter new Eecipes:", self)
-        configurations_label.setStyleSheet(f"font: bold 12px Verdana; color: {c0};")
+        configurations_label = QLabel(widget_title.upper(), self)
+        configurations_label.setStyleSheet(f"font: bold 8px Verdana; color: {c0};")
         layout.addWidget(configurations_label)
 
-        # Add additional components for CRUD functionality here
-        # For example, text input fields, buttons, etc.
         return container
 
 
